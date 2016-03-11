aec1457e0b2b0d144f2a4e1907f7c8ab65644893
879108248@qq.com <Administrator@shixx_PC>
2016-03-11 10:39:06 +0800

    once more~ second~

--- src/com/test1/HelloWorld.java
+++ src/com/test1/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.test1;
 
 public class HelloWorld {
+	
 	public static void main(String[] args) {
 		
 		System.out.println("I love U !!!");
+		System.out.println("HAHA,这件事情我是多虑了呀~你自己可以治愈你自己的吧~come on 加油baby");
 		
 	}
 
